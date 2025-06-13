f7252944f7f100722befb0572e046db90433bcee
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2025-06-13 11:02:38 +0100

    final dia 13

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const session = require("express-session");
+const dotenv = require('dotenv');
+const { MongoClient, ObjectId } = require('mongodb');
+
+dotenv.config();
+
+console.log(process.env.SECRET);
+
+const app = new express();
+
+app.use(session({ secret: process.env.SECRET || "12345" }));
+app.use(express.urlencoded());
+
+//app.use(cookieParser());
+
+
+app.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username);
+    console.log(password);
+
+    const user = await usersCollection.findOne({ username });
+    if (user && user.password === password) {
+        req.session.username = username;
+        //res.cookie('username',username, { maxAge: 900000, httpOnly: true });
+        res.send("ok");
+    } else {
+        res.send("error");
+    }
+})
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+// simula bd com users e passwords
+//let users = {
+//    user1: "password1",
+//    user2: "password2"
+//}
+
+app.get('/', (req, res) => {
+    res.send("olá");
+});
+
+app.get('/protegido', estaAutenticado, (req, res) => {
+        res.send("olá " + username);
+});
+
+function estaAutenticado(req, res, next) {
+    const username = req.session.username;
+    if (username) {
+        next();
+    } else {
+        res.redirect('/login.html')
+    }
+}
+
+app.get('/segredo', estaAutenticado, (req, res) => {
+    res.send("qualqeur coisa")
+});
+
+app.post('/login',  async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username);
+    console.log(password);
+    const user = await usersCollection.findOne({ username });
+    if (user && user.password === password) {
+        req.session.username = username;
+        res.send("ok");
+    } else {
+        res.redirect('/');
+    }
+});
+
+const client = new MongoClient(process.env.MONGOURI);
+
+async function startServer() {
+    await client.connect();
+    const db = client.db('usersdb');
+    usersCollection = db.collection('users');
+    console.log('MongoDB ligado');
+    app.listen(process.env.PORT || 3000, () => console.log('servidor na porta '+ process.env.PORT || 3000));
+  }
+  
+startServer();
